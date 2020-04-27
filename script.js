5ed7bed815b4c00632eb3e65372ebe0e578880dd
Anna Dagbjort <annadagbjort@Annas-MacBook-Pro.local>
2020-04-27 20:51:34 +0000

    header responsive

--- script.js
+++ script.js
@@ -6,9 +6,5 @@ menuIcon.addEventListener('click',() => {
     navbar.classList.toggle("change");
 })
 
-//burger menu changes between normal and X
-//function burgerMenuAnimation(x) {
-//     x.classList.toggle("change");
-// }
 
 //burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
