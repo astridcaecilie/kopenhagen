a06da2b2b773d434c85cb2d2e6279d9fa0543089
Anna Dagbjort <annadagbjort@Annas-MacBook-Pro.local>
2020-04-27 14:13:24 +0000

    logo, burger menu changes from X to normal

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//burger menu changes between normal and X
+function burgerMenuAnimation(x) {
+     x.classList.toggle("change");
+ }
+
+//burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
