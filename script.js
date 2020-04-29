1f75186c29e347ba46c74e08a7c8c5ff07242c60
valeocchi <valeocchi98@gmail.com>
2020-04-29 10:21:03 +0200

    it worked! slideshow done

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
-//const menuIcon = document.querySelector('.burgerMenu');
+const menuIcon = document.querySelector('.burgerMenu');
 
-//const navbar = document.querySelector('.navbar')
+const navbar = document.querySelector('.navbar')
 
-//menuIcon.addEventListener('click',() => {
- //   navbar.classList.toggle("change");
-//})
-
+menuIcon.addEventListener('click',() => {
+    navbar.classList.toggle("change");
+}
 
 //burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
 
