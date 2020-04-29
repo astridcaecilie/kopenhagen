5ce44b3266eea2fb0da8b280c675864ec516807d
astridcaecilie <astrid_chr@hotmail.com>
2020-04-29 11:25:19 +0200

    featured img working - size of them is not :/

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const menuIcon = document.querySelector('.burgerMenu');
 
 const navbar = document.querySelector('.navbar')
 
-menuIcon.addEventListener('click',() => {
+menuIcon.addEventListener('click', () => {
     navbar.classList.toggle("change");
 })
 
@@ -10,12 +10,22 @@ menuIcon.addEventListener('click',() => {
 //burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
 
 
-fetch ("http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event?_embed")
-     .then(res => res.json())
+fetch("http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event?_embed")
+    .then(res => res.json())
     .then(handleData)
 
-function handleData (events){
+function handleData(events) {
     events.forEach(showEvents)
+}
+
+function getArtistName() {
+    fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/tags")
+        .then(res => res.json())
+        .then(handleArtistNames)
+}
+
+function handleArtistNames (categories){
+    categories.forEach(addNavLink); //Not really sure about this
 }
 
 function showEvents(events) {
@@ -30,8 +40,17 @@ function showEvents(events) {
     copy.querySelector(".temp-event-name").textContent = events.title.rendered;
     copy.querySelector(".temp-artist").textContent = events.tags; //only showing tag id instead of name
     copy.querySelector(".temp-paragraph").textContent = events.excerpt.rendered; //showing html tags
-//    copy.querySelector(".temp-image").src = events.images.guid; //not the right way to get featured img
-    copy.querySelector(".temp-date").textContent = events.start_date + events.end_date; // works but needs seperation
+
+    copy.querySelector(".start-date").textContent = events.start_date;
+//    copy.querySelector(".to-date").textContent = events.end_date;
+
+    if (events.end_date == true) {
+        copy.querySelector(".to-date").textContent = events.end_date;
+    } else {
+        copy.querySelector(".to-date").classList.add("hide");
+    }
+
+    //    copy.querySelector(".temp-date").textContent = events.start_date + events.end_date; // works but needs seperation
     copy.querySelector(".temp-image").src = events._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
 
     document.querySelector("main").appendChild(copy);
