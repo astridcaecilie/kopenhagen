ec13005c18d649e965c696b5a970c41e517e1590
valeocchi <valeocchi98@gmail.com>
2020-04-29 12:48:48 +0200

    add bracket

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const navbar = document.querySelector('.navbar')
 
 menuIcon.addEventListener('click',() => {
     navbar.classList.toggle("change");
-}
+})
 
 //burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
 
