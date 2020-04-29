58a049f923d69844b57663f12ee3e4f926e31a1c
Anna Dagbjort <annadagbjort@Annas-MacBook-Pro.local>
2020-04-29 12:00:49 +0000

    different fetch

--- script.js
+++ script.js
@@ -12,46 +12,63 @@ menuIcon.addEventListener('click', () => {
 
 
 
-fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/tags")
+fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event")
     .then(res => res.json())
-    .then(handleTagData)
+    .then(handleEventData)
 
-function handleTagData(handled) {
+function handleEventData(handled) {
     console.log("handle data");
     console.log(handled);
-    handled.forEach(showTagData)
+    handled.forEach(showEventData)
 }
 
-function showTagData(tagData) {
-    //    console.log("show tagData");
-    //    console.log(tagData);
+function showEventData(EventData) {
+    //    console.log("show EventData");
+    //    console.log(EventData);
+
+    const eventId = EventData.id;
+    console.log("artistEventId");
+    console.log(eventId);
+
+    if (eventId) {
+        //single bike
+        fetch("http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event/" + eventId + "?_embed")
+            .then(res => res.json())
+            .then(getInfoById)
+    }
+
+    function getInfoById(info) {
+        var artistName = info._embedded["wp:term"][1][0].name;
+        console.log("artistName");
+        console.log(artistName);
 
 
 
-    const artistName = tagData.name; // Here I get each artists name
-    const artistTagId = tagData.id; // Here I get each artist tag id
-    console.log("artistTagId");
-    console.log(artistTagId);
-
-    const artistNameDisplay = document.createElement("li"); //I create <li>
-
-    artistNameDisplay.classList.add("artistNameDisplay"); //add a class to the <li> so it becomes <li class="artistNameDisplay">
-
-    artistNameDisplay.textContent = artistName; //make the li include the names
-
-    document.querySelector(".artistsFetched").appendChild(artistNameDisplay); //Get the <ul class="artistsFetched"> that alredy excists in the html and put in the new li's
-
-    //Getting the fist letter of each name and make it a class on the element
-    const firstLetter = artistName.charAt(0);
-    console.log(firstLetter)
-    artistNameDisplay.classList.add(firstLetter);
 
 
-// https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories?parent=23&orderby=count&order=desc
+    //    const artistName = EventData.name; // Here I get each artists name
+    //    const artistEventId = EventData.id; // Here I get each artist Event id
+
+
+        const artistNameDisplay = document.createElement("li"); //I create <li>
+
+        artistNameDisplay.classList.add("artistNameDisplay"); //add a class to the <li> so it becomes <li class="artistNameDisplay">
+
+        artistNameDisplay.textContent = artistName; //make the li include the names
+
+        document.querySelector(".artistsFetched").appendChild(artistNameDisplay); //Get the <ul class="artistsFetched"> that alredy excists in the html and put in the new li's
+
+        //Getting the fist letter of each name and make it a class on the element
+        const firstLetter = artistName.charAt(0);
+        console.log(firstLetter)
+        artistNameDisplay.classList.add(firstLetter);
+}
+
+    // https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories?parent=23&orderby=count&order=desc
     // the nationality
 
-   // http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories?post=55
-   // http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/tags?post=55
+    // http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories?post=55
+    // http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/tags?post=55
 
     // http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event/37?_embed
 
