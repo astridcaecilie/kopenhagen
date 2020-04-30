70666336a86973899626ff031a987a9b1a5045e8
astridcaecilie <astrid_chr@hotmail.com>
2020-04-30 15:27:02 +0200

    Got artists to work and removed html tags from excerpt

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
-const menuIcon = document.querySelector('.burgerMenu');
+window.addEventListener("DOMContentLoaded", init);
 
-const navbar = document.querySelector('.navbar')
+function init() {
 
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle("change");
-})
+    getArtistName();
+
+    const menuIcon = document.querySelector('.burgerMenu');
+
+    const navbar = document.querySelector('.navbar')
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle("change");
+    })
 
 
-//burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
+    //burger menu code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
 
 
-fetch("http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event?_embed")
-    .then(res => res.json())
-    .then(handleData)
+    fetch("http://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event?_embed&per_page=100")
+        .then(res => res.json())
+        .then(handleData)
+}
+
+//getting the artist name instead of a ID number
+    function getArtistName() {
+        fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories?_embed")
+            .then(res => res.json())
+            .then(handleArtistNames)
+    }
+
+    function handleArtistNames(addArtist) {
+        addArtist.forEach(addArtistNames);
+    }
+
+    function addArtistNames(artistNames) {
+        console.log(artistNames);
+        //        copy.querySelector(".temp-artist").textContent = artistNames.
+    }
 
 function handleData(events) {
     events.forEach(showEvents)
 }
 
-function getArtistName() {
-    fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/tags")
-        .then(res => res.json())
-        .then(handleArtistNames)
-}
-
-function handleArtistNames (categories){
-    categories.forEach(addNavLink); //Not really sure about this
-}
 
 function showEvents(events) {
-    console.log(events)
+    //    console.log(events)
     const template = document.querySelector("template").content;
 
     const copy = template.cloneNode(true);
 
-    console.log(events.title.rendered)
+    //    console.log(events.title.rendered)
 
+    const artistName = events._embedded["wp:term"][1][0];
+    if (artistName !== undefined) {
+        console.log(artistName.name);
+        copy.querySelector(".temp-artist").textContent = artistName.name; //only showing tag id instead of name
 
-//    copy.querySelector(".temp.institution").textContent = events._embedded["wp:term"][1][0].name;
+    }
+
+    //    console.log(events._embedded["wp:term"][1][0].name);
+
+    //    copy.querySelector(".temp.institution").textContent = events._embedded["wp:term"][1][0].name;
     copy.querySelector(".temp-event-name").textContent = events.title.rendered;
-    copy.querySelector(".temp-artist").textContent = events.tags; //only showing tag id instead of name
-    copy.querySelector(".temp-paragraph").textContent = events.excerpt.rendered; //showing html tags
+    copy.querySelector(".temp-paragraph").innerHTML = events.excerpt.rendered; //showing html tags
+    //    copy.querySelector(".temp-institution").textContent = events._embedded["wp:term"][0][1];
 
     copy.querySelector(".start-date").textContent = events.start_date;
     copy.querySelector(".to-date").textContent = events.end_date;
@@ -49,8 +71,12 @@ function showEvents(events) {
         copy.querySelector(".two-dates").classList.add("hide");
     }
 
-    //    copy.querySelector(".temp-date").textContent = events.start_date + events.end_date; // works but needs seperation
     copy.querySelector(".temp-image").src = events._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
 
+
+
+
+
+    //getting the artist name instead of a ID number - finished
     document.querySelector("main").appendChild(copy);
 }
