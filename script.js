57a028a139d5f050ddb332ede750119eabeb9844
Anna Dagbjort <annadagbjort@Annas-MacBook-Pro.local>
2020-05-04 15:17:24 +0000

    code cleaned up, to merge

--- script.js
+++ script.js
@@ -6,51 +6,39 @@ menuIcon.addEventListener('click', () => {
     navbar.classList.toggle("change");
 }) //burger menu toggles to and from X
 
-//events
+// *** FETCH fetching from both pod types ***
+//fetching from event pods
 fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/event" + "?_embed")
     .then(res => res.json())
     .then(handleData)
-//magazine
+//fetching from magazine pods
 fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/magazine" + "?_embed")
     .then(res => res.json())
     .then(handleData)
-//Categories
-//fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories?per_page=100")
-//    .then(res => res.json())
-//    .then(handleData)
 
 function handleData(handled) {
+    //Create an array and then push the artist names (line31) into the array
     var artistNames = [];
     var categoryIds = [];
     var categoryNames = [];
 
-    //console.log("handled");
-    //    console.log(handled);
-    //console.log(handled._embedded["wp:term"][0]);
     handled.forEach(item => {
         var allCategories = item._embedded["wp:term"][0];
-        //                console.log("item");
-        //                console.log(item);
-
-
 
         //check if there is a tag
         if (item._embedded["wp:term"][1] == false) {
-            // No tag
+            //There is no tag (artist name) in this event/magazine pod
         } else {
-            // Yes tag
+            //Here we have a tag (artist name)
             var names = item._embedded["wp:term"][1][0].name;
-            //                        console.log("names");
-            //                        console.log(names);
         }
+        //so we get the names (line28) and then push it to the var artistName (line21)
         artistNames.push(names);
 
         // Since there are multiple categories I needed one more step to get "inside"
         allCategories.forEach(cate => {
             var catId = cate.id;
             var catName = cate.name;
-
-
 
             //Now I push it outside of this function and create an array from it
             //            console.log(catId);
@@ -60,54 +48,36 @@ function handleData(handled) {
         });
     });
 
-
-//    categoryIds.forEach(fetchingCat);
-//    categoryIds.forEach(compare);
-    //        console.log(categoryIds)
-
-    //categoryNames.forEach(displayCategoryName)
     categoryIds.sort();
     categoryNames.sort();
-//    categoryIds.forEach(noRepeat);
 
-    function noRepeat(array){
+    function noRepeat(array) {
         var noRepeatArray = [];
 
-
         // Loop through array values
-        for(i=0; i < array.length; i++){
-            if(noRepeatArray.indexOf(array[i]) === -1) {
+        for (i = 0; i < array.length; i++) {
+            if (noRepeatArray.indexOf(array[i]) === -1) {
                 noRepeatArray.push(array[i]);
             }
         }
         return noRepeatArray;
 
     }
-//console.log(categoryIds)
 
-//     console.log("categoryNames");
     var catNamesNoRepeat = (noRepeat(categoryNames));
     var catIdsNoRepeat = (noRepeat(categoryIds));
-//    console.log(catNamesNoRepeat);
-//    console.log(catIdsNoRepeat);
 
-//
-
-//    console.log("categoryIds");
-//    console.log();
-//    console.log(categoryIds);
-//    console.log(find_duplicate_in_array(categoryIds));
-//
 
     catIdsNoRepeat.forEach(fetchingCat)
 
+    //Sort the array and for each artist name call the function showTagData
     artistNames.sort();
     artistNames.forEach(showTagData);
 
     function fetchingCat(categories) {
-        fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories/"
-              + categories
-             )
+        fetch("https://astridcaecilie.dk/kopenhagen/wordpress/wp-json/wp/v2/categories/" +
+                categories
+            )
             .then(res => res.json())
             .then(handleCategories)
     }
@@ -123,81 +93,49 @@ function handleData(handled) {
         var theInstitutionLocation = [];
 
 
-
-//                console.log(handledCatData)
-//                console.log("hey")
-
-
-        // For the location
-//
-//        if (handledCatData.parent == 11) {
-//            var locationId = handledCatData.id;
-//            var locationName = handledCatData.name;
-//            //                    console.log(locationId);
-//            //                    console.log(locationName);
-//            theLocationIds.push(locationId);
-//            theLocationNames.push(locationName);
-//            //            console.log(theLocationIds);
-//            //            console.log(theLocationNames);
-//
-//
-//            var regionsUl = document.querySelector(".regionsFetched #" + theLocationNames);
-//            console.log(regionsUl)
-//
-////             console.log("location");
-//        }
-
         //for the Institution name
         if (handledCatData.parent == 2) {
             var institutionId = handledCatData.id;
             var institutionName = handledCatData.name;
-             var institutionLocation = handledCatData.description;
-//             console.log(handledCatData.description);
-            //                    console.log(locationId);
-            //                    console.log(locationName);
+            var institutionLocation = handledCatData.description;
             theInstitutionIds.push(institutionId);
             theInstitutionNames.push(institutionName);
-//            theInstitutionLocation.push(institutionLocation);
-
-            //          console.log(theInstitutionIds);
-//                                    console.log(theInstitutionNames);
 
             var institutionLi = document.createElement("li");
             institutionLi.textContent = institutionName;
-//            console.log(institutionLi);
 
             var regionsUl = document.querySelector(".regionsFetched #" + institutionLocation);
             console.log(regionsUl);
 
             regionsUl.appendChild(institutionLi);
 
-        if (regionsUl > institutionLi) {
-            regionsUl.classList.remove("hide");
-        }
+            if (regionsUl > institutionLi) {
+                regionsUl.classList.remove("hide");
+            }
         }
     }
 }
 
 
 function showTagData(artistNameData) {
-    //    console.log("artistNameData");
-    //    console.log(artistNameData);
+    //create a li with each artist name
+    var li = document.createElement("li");
+    li.textContent = artistNameData;
 
-    var artistLi = document.createElement("li");
-    artistLi.textContent = artistNameData;
-
+    //create an if statement so no empty elements sneak in
     if (artistNameData) {
+        //We get the fist letter of the artist name
         const firstLetter = artistNameData.charAt(0);
-        //artistNameDisplay.classList.add(firstLetter);
 
-        //  console.log(".artistsFetched #" + firstLetter);
+        //Select the correct ul placeholder from the html document and give it the name "elem"
         var elem = document.querySelector(".artistsFetched #" + firstLetter);
 
-        elem.appendChild(artistLi);
+        //append the li tags with the artist name
+        elem.appendChild(li);
 
-        const h2 = document.querySelector(".LetterH2");
-
-        if (elem > artistLi) {
+        //and since all the ul's have the class hide we need to remove it if the ul contains any li's
+        // in css: ** .hide {display:none;} **
+        if (elem > li) {
             elem.classList.remove("hide");
         }
     }
